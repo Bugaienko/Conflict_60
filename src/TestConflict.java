d4e730fe518c9bf2ba37cf26818b292595c55158
SergeyB <sergiibugaienko@gmail.com>
2025-03-19 12:41:22 +0100

    Initial commit

---
+++ src/TestConflict.java
@@ -0,0 +1,15 @@
+/**
+ * @author Sergey Bugaenko
+ * {@code @date} 19.03.2025
+ */
+
+public class TestConflict {
+
+    public void test1() {
+        // Todo task for dev A
+    }
+
+    public void test2() {
+        // Todo task for dev B
+    }
+}
